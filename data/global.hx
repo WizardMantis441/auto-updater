207c3b05601cfc75bc10f02da561ae3eba05f747
WizardMantis441 <77557487+WizardMantis441@users.noreply.github.com>
2024-08-16 02:05:22 -0500

    get the actual folder name instead

--- data/global.hx
+++ data/global.hx
@@ -2,6 +2,6 @@ import Sys;
 
 var updated:Bool = false;
 function preStateSwitch() {
-    if (!updated && Sys.command("cd mods/" + "auto-updater" + " && git pull") == 0)
+    if (!updated && Sys.command("cd mods/" + ModsFolder.currentModFolder + " && git pull") == 0)
         updated = true;
 }
